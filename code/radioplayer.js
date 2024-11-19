c2bb65f023577b748fb9a4a1e099b3293403aa8e
t-kupp <kupper.thorge@gmail.com>
2024-11-19 10:46:29 +0100

    built radio player

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -6,3 +6,44 @@
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
+const channelsWrapper = document.querySelector("#channels-wrapper");
+
+async function getData() {
+  const response = await fetch("http://api.sr.se/api/v2/channels?format=json&size=100");
+  const data = await response.json();
+  console.log(data);
+  displayData(data);
+}
+getData();
+
+function displayData(data) {
+  const slicedData = data.channels.slice(0, 10);
+  console.log(slicedData);
+
+  slicedData.forEach((channel) => {
+    const container = document.createElement("div");
+    container.classList.add("channel-container");
+    channelsWrapper.appendChild(container);
+
+    const image = document.createElement("img");
+    image.classList.add("channel-image");
+    image.src = channel.image;
+    container.appendChild(image);
+
+    const controlsWrapper = document.createElement("div");
+    controlsWrapper.classList.add("controls-wrapper");
+    controlsWrapper.style.backgroundColor = `#${channel.color}`;
+    container.appendChild(controlsWrapper);
+
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.innerText = channel.name;
+    controlsWrapper.appendChild(title);
+
+    const player = document.createElement("audio");
+    player.classList.add("player");
+    player.controls = true;
+    player.src = channel.liveaudio.url;
+    controlsWrapper.appendChild(player);
+  });
+}
